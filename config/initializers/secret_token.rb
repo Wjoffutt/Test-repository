eeee80e448616c873c159ee55aa89f93d1fb62c2
U-Orsius-PC\Orsius <Orsius@Orsius-PC.(none)>
2012-08-14 14:00:33 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstTry::Application.config.secret_token = '29b4f590e9279924a0de5850aa2984d95ebbd948095b0edd3d2a3c473faa028022e865a67e7fc529ca414fb2075c8558cf84f7bea9cfcefb91e95a021bd8856c'
